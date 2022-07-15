c63cbe2f25454138f7659d7e8d9253b6d42a7a02
enorphin82 <enorphin82@gmail.com>
2022-07-15 15:16:09 +0300

    add configs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+
+const {
+    PHASE_DEVELOPMENT_SERVER,
+    PHASE_PRODUCTION_BUILD,
+    PHASE_TEST
+} = require('next/constants')
+
+const nextConfig = (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER
+    // when `next build` or `npm run build` is used
+    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1'
+    // when `next build` or `npm run build` is used
+    const isTesting = phase === PHASE_TEST
+
+    console.log(`isDev:${isDev}  isProd:${isProd}   isTesting:${isTesting}`)
+    const env = {
+        PORT: (() => {
+            if (isDev) return process.env.PORT
+            if (isProd) {
+                return process.env.PORT
+            }
+            if (isTesting) return process.env.PORT
+            return 'RESTURL_SPEAKERS:not (isDev,isProd && !isTesting,isProd && isTesting)'
+        })()
+    }
+    console.log({env})
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV)
+    return {
+        reactStrictMode: true,
+        swcMinify: true,
+        env
+    }
 }
 
 module.exports = nextConfig
